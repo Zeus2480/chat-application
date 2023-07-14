5e2090e180c1d0590373d9ec2b94d43582cb64f8
Faizan Siddiqui <siddiquifaizan248@gamil.com>
2023-07-14 12:57:11 +0530

    First

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import AuthPage from "./AuthPage/index.vue";
+import ChatsPage from "./ChatsPage/index.vue";
+</script>
+
+<template>
+  <AuthPage v-if="!user" @onAuth="handleAuth" />
+  <ChatsPage
+    v-else
+    v-bind:username="user.username"
+    v-bind:secret="user.secret"
+  />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: undefined,
+    };
+  },
+  methods: {
+    handleAuth(user) {
+      this.user = user;
+    },
+  },
+};
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+</style>
